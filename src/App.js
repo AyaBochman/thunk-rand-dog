b2a8a3482ad7c9e64877368dd1d72b31151bc03c
aya <aya.bochman@gmail.com>
2019-06-20 21:41:26 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = ["ride a unicorn", "travel to amsterdam", "finish my blog post", "feed my dog", "workout 3 times", "watch GOT"]
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { found: {} }
+
+  }
+
+  handleChange = (e) => {
+
+    let listItems = this.refs.listItems.childNodes;
+    var foundItem = {};
+    if (e.target.value.length > 2) {
+      listItems.forEach((item, i) => {
+
+        if (item.innerText.toLowerCase().includes(e.target.value)) {
+
+          foundItem[listItems[i].innerText] = true;
+          console.log(foundItem)
+          this.setState({
+            found: foundItem
+          })
+
+        }
+      })
+    }
+
+    if (e.target.value == "") {
+      this.setState({
+        found: {}
+      })
+    }
+  }
+
+  render() {
+
+    return (
+
+      <div className="App">
+
+        <h1>Search in List</h1>
+        <input type={'text'} placeholder={'search...'} onChange={this.handleChange} />
+        <br />
+        <br />
+        <div ref="listItems">
+          {list.map((item, i) => {
+            return <li key={i} className={this.state.found[item] ? 'highlight' : null}>{item}</li>
+          })}
+        </div>
+
+      </div>
+    );
+  }
 }
 
+
 export default App;
+
